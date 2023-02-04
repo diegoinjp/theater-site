a15e9a1f9d9537ae50e9430b10c9fc5aaddf6617
diegoinjp <diego.vpda@gmail.com>
2023-02-04 16:24:53 +0900

    Add card component

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'メルシアーク'
+export const SITE_TITLE = '劇団月のしずく'
 export const SITE_DESCRIPTION = '舞台・殺陣・ダンスのおしゃれな小劇場'
