261ca6bfa468444a49114a4c491e18931494cf84
diegoinjp <diego.vpda@gmail.com>
2023-02-07 18:59:48 +0900

    Add japanese time format

--- src/config.ts
+++ src/config.ts
@@ -3,3 +3,12 @@
 
 export const SITE_TITLE = '劇団月のしずく'
 export const SITE_DESCRIPTION = '舞台・殺陣・ダンスのおしゃれな小劇場'
+
+export const date_options: Intl.DateTimeFormatOptions = {
+  year: 'numeric',
+  month: 'long',
+  day: 'numeric',
+  hour: 'numeric',
+  minute: 'numeric',
+  weekday: 'narrow'
+}
